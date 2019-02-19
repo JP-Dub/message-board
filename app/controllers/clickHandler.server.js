968f08f4f11919758e6988592b7cfbf51d4a1d12
Glitch (boiling-pajama) <none>
2019-02-19 21:11:06 +0000

    😼⛵️ Checkpoint ./app/controllers/clickHandler.server.js:553280/862

--- app/controllers/clickHandler.server.js
+++ app/controllers/clickHandler.server.js
@@ -7,6 +7,14 @@ function ClickHandler() {
 /* /// app.route('/api/threads/:board')  \\\ */  
   this.recentThreads = (req, res) => {
     console.log('recentThreads', req.body, req.params, req.query)
+    Threads
+      .find({board: req.params.board})
+      .exec( (err, threads) => {
+        if(err) throw err;
+      
+        res.json(threads)
+        console.log(threads);
+    });
   };
 /*  
   board  : String,
@@ -30,6 +38,7 @@ function ClickHandler() {
     console.log('createThreads', req.body)
     Threads
       .findOneAndUpdate({board: req.body.board})
+      .sort({'content.thread_id': -1})
       .exec( (err, threads) => {
       if(err) throw err;
       if(!threads) {
@@ -38,11 +47,20 @@ function ClickHandler() {
         board.board = req.body.board;
         board.content.thread_id = '0001';
         board.content.text = req.body.text;
-        board.content.created_on = 
+        board.content.created_on = new Date().toString();
+        board.content.bumped_on = board.content.created_on;
+        board.content.reported = false;
+        board.content.delete_password = req.body.delete_password;
+        board.content.replies = [];
+        
+        // board.save( (err, success) => {
+        //   if(err) throw err;
+        //   console.log(success)
+        // });
       }
     });
     
-    //res.redirect('/b/' + req.body.board)
+    res.redirect('/b/' + req.body.board)
   };
   
   this.reportThreads = (req, res) => {
